6636040868769f60d579186dff1ac1e22c47209c
Satoshi N. M <snmatsutake@yahoo.co.jp>
2017-04-06 23:09:13 -0700

    Initial plugin skeleton
    Started from https://github.com/Bukkit/SamplePlugin
    Updated to use new links and paths

---
+++ src/main/java/io/github/satoshinm/WebSandboxMC/SamplePosCommand.java
@@ -0,0 +1,41 @@
+
+package io.github.satoshinm.WebSandboxMC;
+
+import org.bukkit.Location;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+/**
+ * Handler for the /pos sample command.
+ * @author SpaceManiac
+ */
+public class SamplePosCommand implements CommandExecutor {
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] split) {
+        if (!(sender instanceof Player)) {
+            return false;
+        }
+        Player player = (Player) sender;
+
+        if (split.length == 0) {
+            Location location = player.getLocation();
+            player.sendMessage("You are currently at " + location.getX() +"," + location.getY() + "," + location.getZ() +
+                    " with " + location.getYaw() + " yaw and " + location.getPitch() + " pitch");
+            return true;
+        } else if (split.length == 3) {
+            try {
+                double x = Double.parseDouble(split[0]);
+                double y = Double.parseDouble(split[1]);
+                double z = Double.parseDouble(split[2]);
+
+                player.teleport(new Location(player.getWorld(), x, y, z));
+            } catch (NumberFormatException ex) {
+                player.sendMessage("Given location is invalid");
+            }
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
---
+++ src/main/java/io/github/satoshinm/WebSandboxMC/WebSandboxPlugin.java
@@ -0,0 +1,59 @@
+
+package io.github.satoshinm.WebSandboxMC;
+
+import java.util.HashMap;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Sample plugin for Bukkit
+ *
+ * @author Dinnerbone
+ */
+public class WebSandboxPlugin extends JavaPlugin {
+    private final SamplePlayerListener playerListener = new SamplePlayerListener(this);
+    private final SampleBlockListener blockListener = new SampleBlockListener();
+    private final HashMap<Player, Boolean> debugees = new HashMap<Player, Boolean>();
+
+    @Override
+    public void onDisable() {
+        // TODO: Place any custom disable code here
+
+        // NOTE: All registered events are automatically unregistered when a plugin is disabled
+
+        // EXAMPLE: Custom code, here we just output some info so we can check all is well
+        getLogger().info("Goodbye world!");
+    }
+
+    @Override
+    public void onEnable() {
+        // TODO: Place any custom enable code here including the registration of any events
+
+        // Register our events
+        PluginManager pm = getServer().getPluginManager();
+        pm.registerEvents(playerListener, this);
+        pm.registerEvents(blockListener, this);
+
+        // Register our commands
+        getCommand("pos").setExecutor(new SamplePosCommand());
+        getCommand("debug").setExecutor(new SampleDebugCommand(this));
+
+        // EXAMPLE: Custom code, here we just output some info so we can check all is well
+        PluginDescriptionFile pdfFile = this.getDescription();
+        getLogger().info( pdfFile.getName() + " version " + pdfFile.getVersion() + " is enabled!" );
+    }
+
+    public boolean isDebugging(final Player player) {
+        if (debugees.containsKey(player)) {
+            return debugees.get(player);
+        } else {
+            return false;
+        }
+    }
+
+    public void setDebugging(final Player player, final boolean value) {
+        debugees.put(player, value);
+    }
+}
