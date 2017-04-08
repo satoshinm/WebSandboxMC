6bdb741c3865dae9d7257e3c87bd92860362ab5b
Satoshi N. M <snmatsutake@yahoo.co.jp>
2017-04-08 09:18:32 -0700

    Refactor block translation: toWebBlockType, toBukkitBlockType

--- src/main/java/io/github/satoshinm/WebSandboxMC/ws/WebSocketServerThread.java
+++ src/main/java/io/github/satoshinm/WebSandboxMC/ws/WebSocketServerThread.java
@@ -109,6 +109,126 @@ public final class WebSocketServerThread extends Thread {
         }
     }
 
+    private int toWebBlockType(Material material) {
+        int type;
+        switch (material) {
+            case AIR:
+                type = 0;
+                break;
+            case GRASS:
+                type = 1;
+                break;
+            case SAND:
+                type = 2;
+                break;
+            // TODO: ores, for now, showing as stone
+            case COAL_ORE:
+            case IRON_ORE:
+                //case DIAMOND_ORE:
+                //case EMERALD_ORE:
+            case REDSTONE_ORE:
+            case GLOWING_REDSTONE_ORE:
+            case LAPIS_ORE:
+                //case QUARTZ_ORE:
+            case GOLD_ORE:
+            case STONE:
+                type = 3;
+                break;
+            case BRICK:
+                type = 4;
+                break;
+            case LOG:
+            case LOG_2:
+                type = 5; // wood
+                break;
+            case GRAVEL:
+                type = 6; // cement, close enough
+            case DIRT:
+                type = 7;
+                break;
+            case WOOD:
+                type = 8; // plank
+                break;
+            case SNOW:
+            case SNOW_BLOCK:;
+                type = 9;
+                break;
+            case GLASS:
+                type = 10;
+                break;
+            case COBBLESTONE:
+                type = 11;
+                break;
+            // TODO: light stone (12)
+            // TODO: dark stone (13)
+            case CHEST:
+                type = 14;
+                break;
+            case LEAVES:
+            case LEAVES_2:
+                type = 15;
+                break;
+            // TODO: cloud (16)
+            case DOUBLE_PLANT: // TODO: other double plants, but a lot look like longer long grass
+            case LONG_GRASS:
+                type = 17; // tall grass
+                break;
+            case YELLOW_FLOWER:
+                type = 18;
+                break;
+            case RED_ROSE:
+                type = 19;
+                break;
+            case CHORUS_FLOWER:
+                type = 20;
+                break;
+            // TODO: sunflower (21)
+            // TODO: white flower (22)
+            // TODO: blue flower (23)
+
+            default:
+                System.out.println("unknown block type="+material);
+                // unknown/unsupported becomes cloud
+                // TODO: support more
+                type = 16;
+
+        }
+        return type;
+    }
+
+    private Material toBukkitBlockType(int type) {
+        Material material;
+        // TODO: refactor reverse translation
+        switch (type) {
+            case 0: material = Material.AIR; break;
+            case 1: material = Material.GRASS; break;
+            case 2: material = Material.SAND; break;
+            case 3: material = Material.STONE; break;
+            case 4: material = Material.BRICK; break;
+            case 5: material = Material.LOG; break;
+            case 6: material = Material.GRAVEL; break;
+            case 7: material = Material.DIRT; break;
+            case 8: material = Material.WOOD; break;
+            case 9: material = Material.SNOW_BLOCK; break;
+            case 10: material = Material.GLASS; break;
+            case 11: material = Material.COBBLESTONE; break;
+            //case 12: material = Material. light stone?
+            //case 13: material = Material. dark stone?
+            case 14: material = Material.CHEST; break;
+            case 15: material = Material.LEAVES; break;
+            //case 16: material = Material.clouds; break; // clouds
+            case 17: material = Material.LONG_GRASS; break;
+            case 18: material = Material.YELLOW_FLOWER; break;
+            case 19: material = Material.RED_ROSE; break;
+            case 20: material = Material.CHORUS_FLOWER; break;
+            case 21: material = Material.DOUBLE_PLANT; break; // sunflower
+            //case 22: material = Material.white flower
+            //case 23: material = Material.blue flower
+            default: material = Material.DIAMOND_ORE; // placeholder TODO fix
+        }
+        return material;
+    }
+
     // Handle a command from the client
     public void handle(String string, ChannelHandlerContext ctx) {
         if (string.startsWith("V,")) { // TODO: move to channel active, but it isn't sent there? want initial client connect
@@ -130,89 +250,7 @@ public final class WebSocketServerThread extends Thread {
                 for (int j = -radius; j < radius; ++j) {
                     for (int k = -radius; k < radius; ++k) {
                         Block block = world.getBlockAt(i + x_center, j + y_center, k + z_center);
-                        int type; // = block.getTypeId();
-                        switch (block.getType()) {
-                            case AIR:
-                                type = 0;
-                                break;
-                            case GRASS:
-                                type = 1;
-                                break;
-                            case SAND:
-                                type = 2;
-                                break;
-                            // TODO: ores, for now, showing as stone
-                            case COAL_ORE:
-                            case IRON_ORE:
-                                //case DIAMOND_ORE:
-                                //case EMERALD_ORE:
-                            case REDSTONE_ORE:
-                            case GLOWING_REDSTONE_ORE:
-                            case LAPIS_ORE:
-                                //case QUARTZ_ORE:
-                            case GOLD_ORE:
-                            case STONE:
-                                type = 3;
-                                break;
-                            case BRICK:
-                                type = 4;
-                                break;
-                            case LOG:
-                            case LOG_2:
-                                type = 5; // wood
-                                break;
-                            case GRAVEL:
-                                type = 6; // cement, close enough
-                            case DIRT:
-                                type = 7;
-                                break;
-                            case WOOD:
-                                type = 8; // plank
-                                break;
-                            case SNOW:
-                            case SNOW_BLOCK:;
-                                type = 9;
-                                break;
-                            case GLASS:
-                                type = 10;
-                                break;
-                            case COBBLESTONE:
-                                type = 11;
-                                break;
-                            // TODO: light stone (12)
-                            // TODO: dark stone (13)
-                            case CHEST:
-                                type = 14;
-                                break;
-                            case LEAVES:
-                            case LEAVES_2:
-                                type = 15;
-                                break;
-                            // TODO: cloud (16)
-                            case DOUBLE_PLANT: // TODO: other double plants, but a lot look like longer long grass
-                            case LONG_GRASS:
-                                type = 17; // tall grass
-                                break;
-                            case YELLOW_FLOWER:
-                                type = 18;
-                                break;
-                            case RED_ROSE:
-                                type = 19;
-                                break;
-                            case CHORUS_FLOWER:
-                                type = 20;
-                                break;
-                            // TODO: sunflower (21)
-                            // TODO: white flower (22)
-                            // TODO: blue flower (23)
-
-                            default:
-                                System.out.println("unknown block type="+block.getType());
-                                // unknown/unsupported becomes cloud
-                                // TODO: support more
-                                type = 16;
-
-                        }
+                        int type = toWebBlockType(block.getType());
 
                         response = "B,0,0,"+(i+radius)+","+(j+radius+y_offset)+","+(k+radius)+","+type+"\n";
                         ctx.channel().writeAndFlush(new BinaryWebSocketFrame(Unpooled.copiedBuffer(response.getBytes())));
@@ -251,35 +289,7 @@ public final class WebSocketServerThread extends Thread {
             int z = Integer.parseInt(array[3]);
             int type = Integer.parseInt(array[4]);
 
-            Material material;
-            // TODO: refactor reverse translation
-            switch (type) {
-                case 0: material = Material.AIR; break;
-                case 1: material = Material.GRASS; break;
-                case 2: material = Material.SAND; break;
-                case 3: material = Material.STONE; break;
-                case 4: material = Material.BRICK; break;
-                case 5: material = Material.LOG; break;
-                case 6: material = Material.GRAVEL; break;
-                case 7: material = Material.DIRT; break;
-                case 8: material = Material.WOOD; break;
-                case 9: material = Material.SNOW_BLOCK; break;
-                case 10: material = Material.GLASS; break;
-                case 11: material = Material.COBBLESTONE; break;
-                //case 12: material = Material. light stone?
-                //case 13: material = Material. dark stone?
-                case 14: material = Material.CHEST; break;
-                case 15: material = Material.LEAVES; break;
-                //case 16: material = Material.clouds; break; // clouds
-                case 17: material = Material.LONG_GRASS; break;
-                case 18: material = Material.YELLOW_FLOWER; break;
-                case 19: material = Material.RED_ROSE; break;
-                case 20: material = Material.CHORUS_FLOWER; break;
-                case 21: material = Material.DOUBLE_PLANT; break; // sunflower
-                //case 22: material = Material.white flower
-                //case 23: material = Material.blue flower
-                default: material = Material.DIAMOND_ORE; // placeholder TODO fix
-            }
+            Material material = toBukkitBlockType(type);
             x += -radius + x_center;
             y += -radius + y_center - y_offset;
             z += -radius + z_center;
@@ -292,5 +302,9 @@ public final class WebSocketServerThread extends Thread {
             }
         }
         // TODO: handle more client messages
+    }
+
+    void notifyBlockUpdate(int x, int y, int z, Material material) {
+
     }
 }
