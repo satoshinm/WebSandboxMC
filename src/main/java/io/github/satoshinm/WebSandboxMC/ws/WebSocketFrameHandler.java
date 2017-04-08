8141f557f3d36c82f54eb0aeee1dc7f090bb914b
Satoshi N. M <snmatsutake@yahoo.co.jp>
2017-04-07 22:38:36 -0700

    Send blocks around fixed area from world when client logins in

--- src/main/java/io/github/satoshinm/WebSandboxMC/ws/WebSocketFrameHandler.java
+++ src/main/java/io/github/satoshinm/WebSandboxMC/ws/WebSocketFrameHandler.java
@@ -15,6 +15,7 @@
  */
 package io.github.satoshinm.WebSandboxMC.ws;
 
+import java.util.List;
 import java.util.Locale;
 
 import io.netty.buffer.ByteBuf;
@@ -24,6 +25,9 @@ import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.handler.codec.http.websocketx.BinaryWebSocketFrame;
 import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
 import io.netty.handler.codec.http.websocketx.WebSocketFrame;
+import org.bukkit.Bukkit;
+import org.bukkit.World;
+import org.bukkit.block.Block;
 
 /**
  * Echoes uppercase content of text frames.
@@ -64,12 +68,79 @@ N,1,guest1
             String string = new String(bytes);
             System.out.println("received "+content.capacity()+" bytes: "+string);
 
-            // TODO: handle client messages
+            // TODO: handle more client messages
 
             if (string.startsWith("V,")) { // TODO: move to channel active, but it isn't sent there? want initial client connect
                 String response = "T,Welcome to WebSandboxMC\n";
-                // TODO: send blocks
+
+                List<World> worlds = Bukkit.getServer().getWorlds();
+                response += "T,Worlds loaded: " + worlds.size() + "\n";
+                response += "B,0,0,0,30,0,1\n"; // floating grass block at (0,30,0) in chunk (0,0)
+                response += "K,0,0,0\n"; // update chunk key (0,0) to 0
+                response += "R,0,0\n"; // refresh chunk (0,0)
                 ctx.channel().writeAndFlush(new BinaryWebSocketFrame(Unpooled.copiedBuffer(response.getBytes())));
+
+
+                World world = worlds.get(0);
+
+                // Send blocks around this area
+                int x_center = -85;
+                int y_center = 78;
+                int z_center = 93;
+                int d = 16;
+
+                response = "";
+                for (int i = -d; i < d; ++i) {
+                    for (int j = -d; j < d; ++j) {
+                        for (int k = -d; k < d; ++k) {
+                            Block block = world.getBlockAt(i + x_center, j + y_center, k + z_center);
+                            int type = block.getTypeId();
+                            /*
+                            switch (block.getType()) {
+                                case AIR:
+                                    type = 0;
+                                case GRASS:
+                                    type = 1;
+                                default:
+                                    System.out.println("type="+block.getType());
+                                    type = 3;
+TODO: translate blocks
+
+COAL_ORE
+DIRT
+DOUBLE_PLANT
+GRASS
+GRAVEL
+IRON_ORE
+LEAVES_2
+LOG_2
+LONG_GRASS
+RED_ROSE
+SAND
+STONE
+YELLOW_FLOWER
+
+
+                            }
+                            */
+
+                            response = "B,0,0,"+(i+d)+","+(j+d+30)+","+(k+d)+","+type+"\n";
+                            ctx.channel().writeAndFlush(new BinaryWebSocketFrame(Unpooled.copiedBuffer(response.getBytes())));
+                        }
+                    }
+                }
+                response = "K,0,0,1\n";
+                ctx.channel().writeAndFlush(new BinaryWebSocketFrame(Unpooled.copiedBuffer(response.getBytes())));
+
+                response = "R,0,0\n";
+                ctx.channel().writeAndFlush(new BinaryWebSocketFrame(Unpooled.copiedBuffer(response.getBytes())));
+
+                response = "T,Blocks sent\n";
+                ctx.channel().writeAndFlush(new BinaryWebSocketFrame(Unpooled.copiedBuffer(response.getBytes())));
+
+
+                System.out.println("Sending response: "+response);
+
             }
         } else {
             String message = "unsupported frame type: " + frame.getClass().getName();
