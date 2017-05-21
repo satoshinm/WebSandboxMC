b78ab4429c4c27c7677138afa2da4ee961ecbf09
Satoshi N. M <snmatsutake@yahoo.co.jp>
2017-05-20 23:27:13 -0700

    Get the sponge plugin configuration path

--- src/main/java/io/github/satoshinm/WebSandboxMC/sponge/WebSandboxSpongePlugin.java
+++ src/main/java/io/github/satoshinm/WebSandboxMC/sponge/WebSandboxSpongePlugin.java
@@ -2,9 +2,14 @@ package io.github.satoshinm.WebSandboxMC.sponge;
 
 import com.google.inject.Inject;
 import org.slf4j.Logger;
+import org.spongepowered.api.Game;
+import org.spongepowered.api.config.ConfigDir;
 import org.spongepowered.api.event.Listener;
 import org.spongepowered.api.event.game.state.GameStartedServerEvent;
 import org.spongepowered.api.plugin.Plugin;
+
+import java.io.File;
+import java.nio.file.Path;
 
 @Plugin(id = "websandboxmc", name = "WebSandboxMC", version = "1.6.0")
 public class WebSandboxSpongePlugin {
@@ -12,8 +17,16 @@ public class WebSandboxSpongePlugin {
     @Inject
     private Logger logger;
 
+    @Inject
+    @ConfigDir(sharedRoot = false)
+    private Path configDir;
+
+    @Inject
+    private Game game;
+
     @Listener
     public void onServerStart(GameStartedServerEvent event) {
         logger.info("WebSandboxMC/Sponge starting");
+        logger.info("config path: " + configDir);
     }
 }
